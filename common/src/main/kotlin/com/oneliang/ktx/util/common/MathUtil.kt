3fb41a072efddf3c5fe74596e947f0f3d52d795f
oneliang <582199098@qq.com>
2020-07-22 22:31:51 +0800

    optimize MathUtil.kt

--- common/src/main/kotlin/com/oneliang/ktx/util/common/MathUtil.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/MathUtil.kt
@@ -3,6 +3,43 @@ package com.oneliang.ktx.util.common
 import java.util.*
 
 object MathUtil {
+    fun calculatePiece(number: Long, modulus: Long): Long {
+        return calculatePieceAndRemainder(number, modulus).first
+    }
+
+    fun calculatePiece(number: Int, modulus: Int): Int {
+        return calculatePieceAndRemainder(number, modulus).first
+    }
+
+    fun calculateRemainder(number: Long, modulus: Long): Long {
+        return calculatePieceAndRemainder(number, modulus).second
+    }
+
+    fun calculateRemainder(number: Int, modulus: Int): Int {
+        return calculatePieceAndRemainder(number, modulus).second
+    }
+
+    fun calculatePieceAndRemainder(number: Int, modulus: Int): Pair<Int, Int> {
+        val pair = calculatePieceAndRemainder(number.toLong(), modulus.toLong())
+        return pair.first.toInt() to pair.second.toInt()
+    }
+
+    fun calculatePieceAndRemainder(number: Long, modulus: Long): Pair<Long, Long> {
+        if (number < 0) {
+            error("number must >= 0")
+        }
+        if (modulus <= 0) {
+            error("modulus must > 0")
+        }
+        val remainder = number % modulus
+        val piece = if (remainder == 0L) {
+            number / modulus
+        } else {
+            number / modulus + 1
+        }
+        return piece to remainder
+    }
+
     fun calculateCompose(totalSize: Int, composeSize: Int, outputIndex: Boolean = false): List<Array<Int>> {
         val list = mutableListOf<Array<Int>>()
         calculateCompose(totalSize, composeSize, outputIndex) {
--- common/src/main/kotlin/com/oneliang/ktx/util/common/MathUtilExtensions.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/MathUtilExtensions.kt
@@ -3,3 +3,15 @@ package com.oneliang.ktx.util.common
 fun Int.calculateCompose(composeSize: Int, outputIndex: Boolean = false) = MathUtil.calculateCompose(this, composeSize, outputIndex)
 
 fun Int.calculateCompose(composeSize: Int, outputIndex: Boolean = false, block: (Array<Int>) -> Unit) = MathUtil.calculateCompose(this, composeSize, outputIndex, block)
+
+fun Int.calculatePiece(modulus: Int): Int = MathUtil.calculatePiece(this, modulus)
+
+fun Int.calculateRemainder(modulus: Int): Int = MathUtil.calculateRemainder(this, modulus)
+
+fun Int.calculatePieceAndRemainder(modulus: Int): Pair<Int, Int> = MathUtil.calculatePieceAndRemainder(this, modulus)
+
+fun Long.calculatePiece(modulus: Long): Long = MathUtil.calculatePiece(this, modulus)
+
+fun Long.calculateRemainder(modulus: Long): Long = MathUtil.calculateRemainder(this, modulus)
+
+fun Long.calculatePieceAndRemainder(modulus: Long): Pair<Long, Long> = MathUtil.calculatePieceAndRemainder(this, modulus)
