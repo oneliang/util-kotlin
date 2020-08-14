a3bf3d6fce8cd9fe3c59f008bd9f39a4b35752f6
oneliang <582199098@qq.com>
2020-08-14 18:41:51 +0800

    optimize Operator.kt

--- math/src/main/kotlin/com/oneliang/ktx/util/math/Operator.kt
+++ math/src/main/kotlin/com/oneliang/ktx/util/math/Operator.kt
@@ -12,49 +12,59 @@ object Operator {
         DIVIDE("DIVIDE", "^DIVIDE\\(([\\w]+)/([\\w]+)\\)$")
     }
 
-    fun <K : Any, V : Any> operate(dataMap: Map<K, V>, operateString: String, operateKeyTransform: (key: String) -> K): Double {
+    fun <K : Any, V : Any, R : Any> operate(dataMap: Map<K, V>, operateString: String, operateKeyTransform: (key: String) -> K, operateValueTransform: (value: V) -> R): Double {
         when {
             operateString.startsWith(Operate.ADD.value) -> {
                 val keyList = operateString.parseRegexGroup(Operate.ADD.regex)
-                return if (keyList.size != 2) {
+                val parseKeyPair = parseKeyList(dataMap, keyList, operateKeyTransform, operateValueTransform)
+                return if (parseKeyPair == null) {
                     0.0
                 } else {
-                    val firstDataKey = operateKeyTransform(keyList[0])
-                    val secondDataKey = operateKeyTransform(keyList[1])
-                    dataMap[firstDataKey].toString().toDoubleSafely() + dataMap[secondDataKey].toString().toDoubleSafely()
+                    parseKeyPair.first.toString().toDoubleSafely() + parseKeyPair.second.toString().toDoubleSafely()
                 }
             }
             operateString.startsWith(Operate.MINUS.value) -> {
                 val keyList = operateString.parseRegexGroup(Operate.MINUS.regex)
-                return if (keyList.size != 2) {
+                val parseKeyPair = parseKeyList(dataMap, keyList, operateKeyTransform, operateValueTransform)
+                return if (parseKeyPair == null) {
                     0.0
                 } else {
-                    val firstDataKey = operateKeyTransform(keyList[0])
-                    val secondDataKey = operateKeyTransform(keyList[1])
-                    dataMap[firstDataKey].toString().toDoubleSafely() - dataMap[secondDataKey].toString().toDoubleSafely()
+                    parseKeyPair.first.toString().toDoubleSafely() - parseKeyPair.second.toString().toDoubleSafely()
                 }
             }
             operateString.startsWith(Operate.MULTIPLY.value) -> {
                 val keyList = operateString.parseRegexGroup(Operate.MULTIPLY.regex)
-                return if (keyList.size != 2) {
+                val parseKeyPair = parseKeyList(dataMap, keyList, operateKeyTransform, operateValueTransform)
+                return if (parseKeyPair == null) {
                     0.0
                 } else {
-                    val firstDataKey = operateKeyTransform(keyList[0])
-                    val secondDataKey = operateKeyTransform(keyList[1])
-                    dataMap[firstDataKey].toString().toDoubleSafely() * dataMap[secondDataKey].toString().toDoubleSafely()
+                    parseKeyPair.first.toString().toDoubleSafely() * parseKeyPair.second.toString().toDoubleSafely()
                 }
             }
             operateString.startsWith(Operate.DIVIDE.value) -> {
                 val keyList = operateString.parseRegexGroup(Operate.DIVIDE.regex)
-                return if (keyList.size != 2) {
+                val parseKeyPair = parseKeyList(dataMap, keyList, operateKeyTransform, operateValueTransform)
+                return if (parseKeyPair == null) {
                     0.0
                 } else {
-                    val firstDataKey = operateKeyTransform(keyList[0])
-                    val secondDataKey = operateKeyTransform(keyList[1])
-                    dataMap[firstDataKey].toString().toDoubleSafely() / dataMap[secondDataKey].toString().toDoubleSafely()
+                    parseKeyPair.first.toString().toDoubleSafely() / parseKeyPair.second.toString().toDoubleSafely()
                 }
             }
         }
         return 0.0
+    }
+
+    private fun <K : Any, V : Any, R : Any> parseKeyList(dataMap: Map<K, V>, keyList: List<String>, operateKeyTransform: (key: String) -> K, operateValueTransform: (value: V) -> R): Pair<R, R>? {
+        if (keyList.size != 2) {
+            return null
+        } else {
+            val firstDataKey = operateKeyTransform(keyList[0])
+            val secondDataKey = operateKeyTransform(keyList[1])
+            val firstDataValue = dataMap[firstDataKey] ?: return null
+            val secondDataValue = dataMap[secondDataKey] ?: return null
+            val fixFirstDataValue = operateValueTransform(firstDataValue)
+            val fixSecondDataValue = operateValueTransform(secondDataValue)
+            return fixFirstDataValue to fixSecondDataValue
+        }
     }
 }
--- math/src/main/kotlin/com/oneliang/ktx/util/math/OperatorExtensions.kt
+++ math/src/main/kotlin/com/oneliang/ktx/util/math/OperatorExtensions.kt
@@ -1,5 +1,5 @@
 package com.oneliang.ktx.util.math
 
-fun <K : Any, V : Any> Map<K, V>.operate(operateString: String, operateKeyTransform: (key: String) -> K) = Operator.operate(this, operateString, operateKeyTransform)
+fun <K : Any, V : Any, R : Any> Map<K, V>.operate(operateString: String, operateKeyTransform: (key: String) -> K, operateValueTransform: (value: V) -> R) = Operator.operate(this, operateString, operateKeyTransform, operateValueTransform)
 
-fun <V : Any> Map<String, V>.operate(operateString: String) = this.operate(operateString) { key: String -> key }
+fun <V : Any> Map<String, V>.operate(operateString: String) = this.operate(operateString, { key: String -> key }) { it }
