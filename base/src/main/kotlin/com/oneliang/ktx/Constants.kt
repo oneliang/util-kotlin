77d89c3cd319896998295731dc09d75b13e2131f
tongxiaoyi <1203371441@qq.com>
2020-08-27 18:57:22 +0800

    add  hour:minute:second   to Constants.Time

--- base/src/main/kotlin/com/oneliang/ktx/Constants.kt
+++ base/src/main/kotlin/com/oneliang/ktx/Constants.kt
@@ -27,6 +27,7 @@ object Constants {
         const val YEAR_MONTH_DAY = "yyyy-MM-dd"// year-month-day
         const val YEAR_MONTH_DAY_HOUR_MINUTE_SECOND = "yyyy-MM-dd HH:mm:ss"//always used for database
         const val YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_MILLISECOND = "yyyy-MM-dd HH:mm:ss,SSS"//always used for log
+        const val HOUR_MINUTE_SECOND = "HH:mm:ss"//hour:minute:second
         const val DEFAULT_DATE_FORMAT = "EEE MMM dd HH:mm:ss zzz yyyy"
         const val UNION_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_MILLISECOND = "yyyyMMddHHmmssSSS"
         const val UNION_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND = "yyyyMMddHHmmss"
