dc66f9e7674eb130c6f9af4ad14128f947309588
oneliang <582199098@qq.com>
2020-09-10 19:12:31 +0800

    +ReentrantLockDataContainer.kt

---
+++ concurrent/src/main/kotlin/com/oneliang/ktx/util/concurrent/ReentrantLockDataContainer.kt
@@ -0,0 +1,52 @@
+package com.oneliang.ktx.util.concurrent
+
+import com.oneliang.ktx.util.common.perform
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+
+class ReentrantLockDataContainer<K : Any, V : Any> {
+
+    private val lock = ReentrantLock()
+    private val map = ConcurrentHashMap<K, V>()
+    private val reentrantLockMap = ConcurrentHashMap<K, ReentrantLock>()
+
+    operator fun set(key: K, block: () -> V): V? {
+        return if (this.reentrantLockMap.containsKey(key)) {
+            val reentrantLock = this.reentrantLockMap[key] ?: error("not found ReentrantLock for key:$key, may be something error")
+            doLockBlock(key, reentrantLock, block)
+        } else {
+            this.lock.lock()
+            if (!this.reentrantLockMap.containsKey(key)) {
+                this.reentrantLockMap[key] = ReentrantLock()
+            }
+            val reentrantLock = this.reentrantLockMap[key] ?: error("not found ReentrantLock for key:$key, may be something error")
+            val result = doLockBlock(key, reentrantLock, block)
+            this.lock.unlock()
+            result
+        }
+    }
+
+    operator fun get(key: K): V? {
+        val reentrantLock = this.reentrantLockMap[key] ?: return null
+        perform({
+            reentrantLock.lock()
+            return this.map[key]
+        }, finally = {
+            reentrantLock.unlock()
+        })
+        return null
+    }
+
+    private fun doLockBlock(key: K, reentrantLock: ReentrantLock, block: () -> V): V? {
+        var result: V? = null
+        perform({
+            reentrantLock.lock()
+            val blockResult = block()
+            this.map[key] = blockResult
+            result = blockResult
+        }, finally = {
+            reentrantLock.unlock()
+        })
+        return result
+    }
+}
