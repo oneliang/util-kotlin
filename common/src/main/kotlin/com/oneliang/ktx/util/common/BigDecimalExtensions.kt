27baeca1861f2b92ae713a8e1db1efe376095eff
oneliang <582199098@qq.com>
2020-10-31 09:55:40 +0800

    optimize some code

--- common/src/main/kotlin/com/oneliang/ktx/util/common/BigDecimalExtensions.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/BigDecimalExtensions.kt
@@ -3,17 +3,17 @@ package com.oneliang.ktx.util.common
 import java.math.BigDecimal
 
 fun BigDecimal.addByBigDecimal(double: Double): BigDecimal {
-    return this.plus(double.toBigDecimal())
+    return this + double.toBigDecimal()
 }
 
 fun BigDecimal.minusByBigDecimal(double: Double): BigDecimal {
-    return this.minus(double.toBigDecimal())
+    return this - double.toBigDecimal()
 }
 
 fun BigDecimal.multiplyByBigDecimal(double: Double): BigDecimal {
-    return this.times(double.toBigDecimal())
+    return this * double.toBigDecimal()
 }
 
 fun BigDecimal.divideByBigDecimal(double: Double): BigDecimal {
-    return this.div(double.toBigDecimal())
+    return this / double.toBigDecimal()
 }
