1202fabe067055478affd79d49f52c46a333eb7e
oneliang <582199098@qq.com>
2020-12-05 10:06:02 +0800

    optimize DateExtensions.kt

--- common/src/main/kotlin/com/oneliang/ktx/util/common/DateExtensions.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/DateExtensions.kt
@@ -10,24 +10,64 @@ fun Date.toFormatString(format: String = Constants.Time.YEAR_MONTH_DAY_HOUR_MINU
     return simpleDateFormat.format(this)
 }
 
-fun Date.getZeroTime(modulusTime: Long): Long {
-    val currentTime = this.time//time zone is zero +0000
+fun Long.getZeroTime(modulusTime: Long): Long {
+    val currentTime = this//time zone is zero +0000
     val timeZoneMilliSecondOffset = TimeZoneUtil.getTimeZoneMilliSecondOffset()
     val currentTimeZoneTime = currentTime + timeZoneMilliSecondOffset
     val retainTime = currentTimeZoneTime % modulusTime//current time zone time
     return currentTimeZoneTime - retainTime - timeZoneMilliSecondOffset//recovery to 0 time zone
 }
 
+fun Long.getDayZeroTime(): Long {
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_DAY)
+}
+
+fun Long.getHourZeroTime(): Long {
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_HOUR)
+}
+
+fun Long.getMinuteZeroTime(): Long {
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_MINUTE)
+}
+
+fun Long.getDayZeroTimeNext(offset: Int = 1): Long {
+    return this.getDayZeroTime() + offset * Constants.Time.MILLISECONDS_OF_DAY
+}
+
+fun Long.getHourZeroTimeNext(offset: Int = 1): Long {
+    return this.getHourZeroTime() + offset * Constants.Time.MILLISECONDS_OF_HOUR
+}
+
+fun Long.getMinuteZeroTimeNext(offset: Int = 1): Long {
+    return this.getMinuteZeroTime() + offset * Constants.Time.MILLISECONDS_OF_MINUTE
+}
+
+fun Long.getDayZeroTimePrevious(offset: Int = 1): Long {
+    return this.getDayZeroTime() - offset * Constants.Time.MILLISECONDS_OF_DAY
+}
+
+fun Long.getHourZeroTimePrevious(offset: Int = 1): Long {
+    return this.getHourZeroTime() - offset * Constants.Time.MILLISECONDS_OF_HOUR
+}
+
+fun Long.getMinuteZeroTimePrevious(offset: Int = 1): Long {
+    return this.getMinuteZeroTime() - offset * Constants.Time.MILLISECONDS_OF_MINUTE
+}
+
+fun Date.getZeroTime(modulusTime: Long): Long {
+    return this.time.getZeroTime(modulusTime)
+}
+
 fun Date.getDayZeroTime(): Long {
-    return getZeroTime(Constants.Time.MILLISECONDS_OF_DAY)
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_DAY)
 }
 
 fun Date.getHourZeroTime(): Long {
-    return getZeroTime(Constants.Time.MILLISECONDS_OF_HOUR)
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_HOUR)
 }
 
 fun Date.getMinuteZeroTime(): Long {
-    return getZeroTime(Constants.Time.MILLISECONDS_OF_MINUTE)
+    return this.getZeroTime(Constants.Time.MILLISECONDS_OF_MINUTE)
 }
 
 fun Date.getDayZeroTimeNext(offset: Int = 1): Long {
