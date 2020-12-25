bb435329d63bf5e313b1ce4814c9a990bc27d6cc
oneliang <582199098@qq.com>
2020-12-25 09:49:34 +0800

    fix compile error

--- jxl/src/test/kotlin/com/oneliang/ktx/util/jxl/test/TestJxlUtil.kt
+++ jxl/src/test/kotlin/com/oneliang/ktx/util/jxl/test/TestJxlUtil.kt
@@ -11,19 +11,19 @@ import java.io.File
 fun main() {
     val fullFilename = "/D:/Dandelion/java/githubWorkspace/util-kotlin/jxl/src/test/kotlin/test.xls"
     val writeFullFilename = "/D:/Dandelion/java/githubWorkspace/util-kotlin/jxl/src/test/kotlin/test_write.xls"
-    val list = File(fullFilename).readSimpleExcel(headerRowIndex = 0, dataRowOffset = 1)
+    val readResult = File(fullFilename).readSimpleExcel(headerRowIndex = 0, dataRowOffset = 1)
     val headers = arrayOf("列1", "列2", "列3", "列4", "列5", "列6", "列7")
     val mapping = mapOf(
-            "列1" to 0,
-            "列2" to 1,
-            "列3" to 2,
-            "列4" to 3,
-            "列5" to 4,
-            "列6" to 5,
-            "列7" to 6
+        "列1" to 0,
+        "列2" to 1,
+        "列3" to 2,
+        "列4" to 3,
+        "列5" to 4,
+        "列6" to 5,
+        "列7" to 6
     )
     val newList = mutableListOf<Array<String>>()
-    list.forEachIndexed { row, data ->
+    readResult.dataList.forEachIndexed { row, data ->
         newList += data.toArray(mapping, Constants.String.BLANK)
         data.forEach { (key, value) ->
             println("$row:$key:$value")
