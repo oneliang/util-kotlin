efb731fa5d0aa9cb901e821c89ecd4efbf64f9e7
oneliang <582199098@qq.com>
2021-03-17 18:46:15 +0800

    refactor the code to frame

--- math/src/main/kotlin/com/oneliang/ktx/util/math/algebra/LossFunction.kt
+++
@@ -1,9 +0,0 @@
-package com.oneliang.ktx.util.math.algebra
-
-import kotlin.math.pow
-
-object LossFunction {
-    fun loss(calculateY: Double, realY: Double): Double {
-        return (calculateY - realY).pow(2)
-    }
-}
