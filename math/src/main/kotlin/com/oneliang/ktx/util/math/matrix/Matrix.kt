08fa8c6d8a3644f41d78756cb4517c562e1f5886
oneliang <582199098@qq.com>
2021-03-23 08:53:33 +0800

    optimize Matrix

--- math/src/main/kotlin/com/oneliang/ktx/util/math/matrix/Matrix.kt
+++ math/src/main/kotlin/com/oneliang/ktx/util/math/matrix/Matrix.kt
@@ -1,6 +1,5 @@
 package com.oneliang.ktx.util.math.matrix
 
-import com.oneliang.ktx.Constants
 import com.oneliang.ktx.util.common.sumByDoubleIndexed
 
 fun matrixAdd(aMatrix: Array<Array<Double>>, bMatrix: Array<Array<Double>>, negative: Boolean = false): Array<Array<Double>> {
@@ -30,7 +29,7 @@ fun Array<Array<Double>>.add(bMatrix: Array<Array<Double>>): Array<Array<Double>
 
 fun Array<Array<Double>>.minus(bMatrix: Array<Array<Double>>): Array<Array<Double>> = matrixMinus(this, bMatrix)
 
-fun matrixMultiply(aMatrix: Array<Double>, bMatrix: Array<Array<Double>>): Array<Double> {
+fun matrixMultiply(aMatrix: Array<Double>, bMatrix: Array<Array<Double>>, transform: (result: Double) -> Double = { it }): Array<Double> {
     if (aMatrix.isEmpty() || bMatrix.isEmpty()) {
         return emptyArray()
     }
@@ -40,24 +39,25 @@ fun matrixMultiply(aMatrix: Array<Double>, bMatrix: Array<Array<Double>>): Array
         for (bY in bMatrix.indices) {
             resultMatrix[x] += aMatrix[bY] * bMatrix[bY][x]
         }
+        resultMatrix[x] = transform(resultMatrix[x])//transform result
     }
     return resultMatrix
 }
 
-fun matrixMultiply(aMatrix: Array<Array<Double>>, bMatrix: Array<Array<Double>>): Array<Array<Double>> {
+fun matrixMultiply(aMatrix: Array<Array<Double>>, bMatrix: Array<Array<Double>>, transform: (result: Double) -> Double = { it }): Array<Array<Double>> {
     if (aMatrix.isEmpty() || bMatrix.isEmpty()) {
         return emptyArray()
     }
     val resultMatrix = Array(aMatrix.size) { Array(bMatrix[0].size) { 0.0 } }
     for (y in resultMatrix.indices) {
-        resultMatrix[y] = aMatrix[y].multiply(bMatrix)
+        resultMatrix[y] = aMatrix[y].multiply(bMatrix, transform)
     }
     return resultMatrix
 }
 
-fun Array<Double>.multiply(bMatrix: Array<Array<Double>>): Array<Double> = matrixMultiply(this, bMatrix)
+fun Array<Double>.multiply(bMatrix: Array<Array<Double>>, transform: (result: Double) -> Double = { it }): Array<Double> = matrixMultiply(this, bMatrix, transform)
 
-fun Array<Array<Double>>.multiply(bMatrix: Array<Array<Double>>): Array<Array<Double>> = matrixMultiply(this, bMatrix)
+fun Array<Array<Double>>.multiply(bMatrix: Array<Array<Double>>, transform: (result: Double) -> Double = { it }): Array<Array<Double>> = matrixMultiply(this, bMatrix, transform)
 
 fun Array<Double>.innerProduct(bMatrix: Array<Double>): Double {
     if (this.isEmpty() || bMatrix.isEmpty()) {
@@ -86,13 +86,16 @@ fun Array<Array<Double>>.innerProduct(bMatrix: Array<Array<Double>>): Double {
 }
 
 fun main() {
-    val aMatrix = arrayOf(arrayOf(1.0, 3.0, 2.0), arrayOf(4.0, 0.0, 1.0))
-    val bMatrix = arrayOf(arrayOf(1.0, 3.0), arrayOf(0.0, 1.0), arrayOf(5.0, 2.0))
+//    f:-0.733928,w:1.0
+//    f:9.098687,w:1.0
+//    f:1.0,w:1.0
+    val aMatrix = arrayOf(-0.733928, 9.098687, 1.0)
+    val bMatrix = arrayOf(arrayOf(1.0), arrayOf(1.0), arrayOf(1.0))
     val resultMatrix = matrixMultiply(aMatrix, bMatrix)
     resultMatrix.forEach { row ->
-        row.forEach {
-            print(it.toString() + Constants.String.TAB_STRING)
-        }
-        println()
+//        row.forEach {
+//            print(it.toString() + Constants.String.TAB_STRING)
+//        }
+        println(row)
     }
 }
