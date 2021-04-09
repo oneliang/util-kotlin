23672066b39371d0b2664c49b6d7ab1ec5678842
oneliang <582199098@qq.com>
2021-04-09 14:20:57 +0800

    +PrimitiveWrapper.kt

---
+++ base/src/main/kotlin/com/oneliang/ktx/pojo/PrimitiveWrapper.kt
@@ -0,0 +1,11 @@
+package com.oneliang.ktx.pojo
+
+class BooleanWrapper(val value: Boolean)
+
+class ByteWrapper(val value: Byte)
+
+class IntWrapper(val value: Int)
+
+class LongWrapper(val value: Long)
+
+class DoubleWrapper(val value: Double)
