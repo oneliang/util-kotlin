d4da0a70ad501ed776fa4b890170a1d0041d2498
oneliang <582199098@qq.com>
2021-11-04 17:55:08 +0800

    optimize MappingJsonKotlinClassProcessor.kt

--- json/src/main/kotlin/com/oneliang/ktx/util/json/MappingJsonKotlinClassProcessor.kt
+++ json/src/main/kotlin/com/oneliang/ktx/util/json/MappingJsonKotlinClassProcessor.kt
@@ -10,14 +10,14 @@ open class MappingJsonKotlinClassProcessor : DefaultJsonKotlinClassProcessor() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : Any, SP> changeClassProcess(kClass: KClass<T>, values: Array<String>, fieldName: String, specialParameter: SP?): Any? {
-        return if (specialParameter != null && specialParameter is Map<*, *>) {
+        return if (specialParameter != null && specialParameter is Map<*, *> && specialParameter.isNotEmpty()) {
             try {
                 val fieldNameKClassMapping = specialParameter as Map<String, Pair<Type, KClass<*>>>
                 if (fieldNameKClassMapping.containsKey(fieldName)) {
                     val (type, fieldNameKClass) = fieldNameKClassMapping[fieldName]!!
                     when (type) {
-                        Type.OBJECT -> values[0].jsonToObject(fieldNameKClass)
-                        Type.ARRAY -> values[0].jsonToObjectList(fieldNameKClass, this)
+                        Type.OBJECT -> values[0].jsonToObject(fieldNameKClass, this, specialParameter)
+                        Type.ARRAY -> values[0].jsonToObjectList(fieldNameKClass, this, specialParameter)
                     }
                 } else {
                     super.changeClassProcess(kClass, values, fieldName, specialParameter)
