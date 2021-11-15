34ef5d4a952817df72bbaa4e46be330cea527a7c
oneliang <582199098@qq.com>
2021-11-15 11:24:00 +0800

    upgrade kotlin version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
     const val group = "com.oneliang.ktx"
     const val version = "1.0"
-    const val kotlinVersion = "1.4.21"
-    const val kotlinxCoroutinesVersion = "1.4.2"
+    const val kotlinVersion = "1.5.31"
+    const val kotlinxCoroutinesVersion = "1.5.2"
 }
