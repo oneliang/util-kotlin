7b7bbf2bff0aec67c84a0bfe9f1699960b785ffd
oneliang <582199098@qq.com>
2021-12-01 16:09:55 +0800

    optimize JsonWebToken.kt

--- jwt/src/main/kotlin/com/oneliang/ktx/util/jwt/JsonWebToken.kt
+++ jwt/src/main/kotlin/com/oneliang/ktx/util/jwt/JsonWebToken.kt
@@ -7,15 +7,15 @@ object JsonWebToken {
 
     const val JWT = "JWT"
 
-    fun create(header: String, body: String, block: (signatureString: String) -> String): String {
-        val headerBase64UrlEncode = Base64UrlCodec.encode(header.toByteArray(Charsets.US_ASCII))
-        val bodyBase64UrlEncode = Base64UrlCodec.encode(body.toByteArray(Charsets.US_ASCII))
+    fun create(headerByteArray: ByteArray, bodyByteArray: ByteArray, block: (signatureString: String) -> ByteArray): String {
+        val headerBase64UrlEncode = Base64UrlCodec.encode(headerByteArray)
+        val bodyBase64UrlEncode = Base64UrlCodec.encode(bodyByteArray)
         val needToSignatureString = headerBase64UrlEncode + Constants.Symbol.DOT_CHAR + bodyBase64UrlEncode
-        val signature = block(needToSignatureString)
-        return needToSignatureString + Constants.Symbol.DOT_CHAR + if (signature.isBlank()) {
+        val signatureByte = block(needToSignatureString)
+        return needToSignatureString + Constants.Symbol.DOT_CHAR + if (signatureByte.isEmpty()) {
             Constants.String.BLANK
         } else {
-            Base64UrlCodec.encode(signature.toByteArray(Charsets.US_ASCII))
+            Base64UrlCodec.encode(signatureByte)
         }
     }
 }
