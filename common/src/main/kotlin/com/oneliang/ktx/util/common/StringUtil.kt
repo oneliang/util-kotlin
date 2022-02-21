494b7562aeaf44aa2ab6e2e69b964b44e24931c8
oneliang <582199098@qq.com>
2022-02-21 15:04:15 +0800

    optimize some code

--- common/src/main/kotlin/com/oneliang/ktx/util/common/StringUtil.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/StringUtil.kt
@@ -2,14 +2,16 @@ package com.oneliang.ktx.util.common
 
 import com.oneliang.ktx.Constants
 
+fun generateZeroString(length: Int) = StringUtil.generateZeroString(length)
+
 object StringUtil {
 
     /**
-     * fill zero
+     * generate zero string
      * @param length
      * @return String
      */
-    fun fillZero(length: Int): String {
+    fun generateZeroString(length: Int): String {
         val stringBuilder = StringBuilder()
         for (i in 0 until length) {
             stringBuilder.append(Constants.String.ZERO)
--- common/src/main/kotlin/com/oneliang/ktx/util/common/StringUtilExtensions.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/StringUtilExtensions.kt
@@ -1,9 +1,18 @@
 package com.oneliang.ktx.util.common
 
 fun Long.toFillZeroString(maxLength: Int): String {
-    return StringUtil.fillZero(maxLength - this.toString().length) + this.toString()
+    val longString = this.toString()
+    return generateZeroString(maxLength - longString.length) + longString
 }
 
 fun Int.toFillZeroString(maxLength: Int): String {
     return this.toLong().toFillZeroString(maxLength)
+}
+
+fun String.fillZeroPrefix(length: Int): String {
+    return generateZeroString(length) + this
+}
+
+fun String.fillZeroSuffix(length: Int): String {
+    return this + generateZeroString(length)
 }
