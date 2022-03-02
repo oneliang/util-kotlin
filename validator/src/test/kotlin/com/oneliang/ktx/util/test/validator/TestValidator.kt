6205b1570b6394a0fc9d9fce5fb751b9f3cd7207
oneliang <582199098@qq.com>
2022-03-02 14:10:46 +0800

    optimize some test about validator

--- validator/src/test/kotlin/com/oneliang/ktx/util/test/validator/TestValidator.kt
+++ validator/src/test/kotlin/com/oneliang/ktx/util/test/validator/TestValidator.kt
@@ -1,12 +1,14 @@
 package com.oneliang.ktx.util.test.validator
 
 import com.oneliang.ktx.util.validator.validate
+import com.oneliang.ktx.util.validator.validateSimply
 
 fun main() {
     val testModel = TestModel().apply {
         this.name = "n"
         this.email = "a@a.com"
     }
+    println(testModel.validateSimply())
     testModel.validate().forEach {
         println(it.fieldName + "," + it.result)
     }
