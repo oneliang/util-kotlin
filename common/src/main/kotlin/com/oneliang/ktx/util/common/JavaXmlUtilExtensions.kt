2796c0503e5d977f417112eea20201e3e5b4778b
oneliang <582199098@qq.com>
2022-03-09 16:42:58 +0800

    optimize some name

--- common/src/main/kotlin/com/oneliang/ktx/util/common/JavaXmlUtilExtensions.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/JavaXmlUtilExtensions.kt
@@ -1,6 +1,7 @@
 package com.oneliang.ktx.util.common
 
 import org.w3c.dom.Document
+import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.InputStream
 import kotlin.reflect.KClass
@@ -9,6 +10,8 @@ fun <T : Any> String.xmlToObject(kClass: KClass<T>, mapping: Map<String, String>
 
 fun <T : Any> String.xmlToObjectList(xmlObjectTag: String, kClass: KClass<T>, mapping: Map<String, String>, classProcessor: KotlinClassUtil.KotlinClassProcessor = JavaXmlUtil.DEFAULT_CLASS_PROCESSOR): List<T> = JavaXmlUtil.xmlToObjectList(this, xmlObjectTag, kClass, mapping, classProcessor)
 
-fun InputStream.parseXml(): Document = JavaXmlUtil.parse(this)
+fun InputStream.toJavaXmlDocument(): Document = JavaXmlUtil.parse(this)
 
-fun File.parseXml(): Document = JavaXmlUtil.parse(this)
+fun File.toJavaXmlDocument(): Document = JavaXmlUtil.parse(this)
+
+fun ByteArray.toJavaXmlDocument(): Document = ByteArrayInputStream(this).toJavaXmlDocument()
