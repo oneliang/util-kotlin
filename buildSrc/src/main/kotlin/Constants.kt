91372cb9ad78d62f80965dbfee9a1faef7b603c0
oneliang <oneliang@microsteel.cn>
2023-03-28 12:33:51 +0800

    upgrade kotlin version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
     const val group = "com.oneliang.ktx"
     const val version = "1.0"
-    const val kotlinVersion = "1.5.31"
-    const val kotlinxCoroutinesVersion = "1.5.2"
+    const val kotlinVersion = "1.8.0"
+    const val kotlinxCoroutinesVersion = "1.6.4"
 }
