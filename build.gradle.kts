30403cdfef16aa145e5e92853f26501b1080b362
oneliang <oneliang@microsteel.cn>
2023-03-29 09:51:49 +0800

    fix compile error

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Constants.kotlinVersion}")
+//        classpath("org.jetbrains.kotlin:kotlin-reflect:${Constants.kotlinVersion}")
+//        classpath("org.jetbrains.kotlin:kotlin-stdlib:${Constants.kotlinVersion}")
         classpath("com.oneliang.ktx:gradle-ext:1.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,10 +10,4 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("com.oneliang.ktx:util-kotlin-base:1.0")
-    implementation("com.oneliang.ktx:util-kotlin-common:1.0")
-    implementation("com.oneliang.ktx:util-kotlin-file:1.0")
-    implementation("com.oneliang.ktx:gradle-ext:1.0")
-    implementation("com.oneliang.ktx:util-kotlin-json:1.0")
-    implementation("com.oneliang.ktx:util-kotlin-generate:1.0")
 }
