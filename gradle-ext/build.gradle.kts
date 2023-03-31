cdf1b7a56725175702121254128c991dfe6c855f
oneliang <oneliang@microsteel.cn>
2023-03-31 18:59:05 +0800

    optimize script

--- gradle-ext/build.gradle.kts
+++ gradle-ext/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version Constants.kotlinVersion
     `maven-publish`
     `kotlin-dsl`
 }
@@ -17,7 +17,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Constants.kotlinVersion}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
---
+++ gradle-ext/buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenLocal()
+    google()
+    mavenCentral()
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
+
+dependencies {
+}
+
