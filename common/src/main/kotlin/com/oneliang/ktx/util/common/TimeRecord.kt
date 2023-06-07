f667236ab88bfe4385d62247b9459eb4f847decf
oneliang <oneliang@microsteel.cn>
2023-06-07 09:44:41 +0800

    optimize common module about TimeRecord

--- common/src/main/kotlin/com/oneliang/ktx/util/common/TimeRecord.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/TimeRecord.kt
@@ -1,17 +1,37 @@
 package com.oneliang.ktx.util.common
 
-class TimeRecord(private val startTimeProvider: () -> Long, private val stopTimeProvider: () -> Long) {
+class TimeRecord(
+    private val startTimeProvider: () -> Long,
+    private val stopTimeProvider: () -> Long,
+    private val recordCallback: (category: String, recordTime: Long) -> Unit = { _, _ -> }
+) {
+    enum class Category {
+        START, STEP_RECORD, RECORD
+    }
 
     private var beginTime = 0L
+    private var stepBeginTime = 0L
 
     fun start() {
         this.beginTime = this.startTimeProvider()
+        this.stepBeginTime = this.beginTime
+        this.recordCallback(Category.START.name, 0L)
+    }
+
+    fun stepRecord(): Long {
+        val newStepBeginTime = this.stopTimeProvider()
+        val stepRecordTime = newStepBeginTime - this.stepBeginTime
+        this.stepBeginTime = newStepBeginTime
+        this.recordCallback(Category.STEP_RECORD.name, stepRecordTime)
+        return stepRecordTime
     }
 
     /**
      * @return Long, return cost time
      */
-    fun stop(): Long {
-        return this.stopTimeProvider() - this.beginTime
+    fun record(): Long {
+        val recordTime = this.stopTimeProvider() - this.beginTime
+        this.recordCallback(Category.RECORD.name, recordTime)
+        return recordTime
     }
 }
--- common/src/test/kotlin/com/oneliang/ktx/util/test/common/TestTimeRecord.kt
+++ common/src/test/kotlin/com/oneliang/ktx/util/test/common/TestTimeRecord.kt
@@ -3,8 +3,11 @@ package com.oneliang.ktx.util.test.common
 import com.oneliang.ktx.util.common.TimeRecord
 
 fun main() {
-    val timeRecord = TimeRecord(System::nanoTime, System::nanoTime)
+    val timeRecord = TimeRecord(System::nanoTime, System::nanoTime) { category, recordTime ->
+        println("%s, cost:%s".format(category, recordTime))
+    }
     timeRecord.start()
     Thread.sleep(1000)
-    print(timeRecord.stop())
+    timeRecord.stepRecord()
+    timeRecord.record()
 }
