d8a1e976eda9c640314867cf4bc3d71f2d5a3adb
oneliang <oneliang@microsteel.cn>
2023-06-11 08:56:20 +0800

    +optimize some test

--- common/src/test/kotlin/com/oneliang/ktx/util/test/common/TestLargeBitSet.kt
+++ common/src/test/kotlin/com/oneliang/ktx/util/test/common/TestLargeBitSet.kt
@@ -5,9 +5,11 @@ import com.oneliang.ktx.util.common.toBinaryString
 import com.oneliang.ktx.util.common.toByteArray
 
 fun main(){
+    println((64 - 1 shr 6).toInt() + 1)
     val largeBitSet = LargeBitSet(1000)
     largeBitSet.set(1)
-    largeBitSet.set(100)
+    largeBitSet.set(64)
     largeBitSet.set(101)
+    largeBitSet.set(500)
     println( (100L).toByteArray().toBinaryString())
 }
