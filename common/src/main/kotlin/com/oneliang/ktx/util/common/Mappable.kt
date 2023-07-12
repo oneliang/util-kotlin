789325eae65ab282cd935f912ad29884f57341a1
oneliang <oneliang@microsteel.cn>
2023-07-12 13:44:12 +0800

    optimize some code

--- common/src/main/kotlin/com/oneliang/ktx/util/common/Mappable.kt
+++ common/src/main/kotlin/com/oneliang/ktx/util/common/Mappable.kt
@@ -1,5 +1,7 @@
 package com.oneliang.ktx.util.common
 
+import java.lang.annotation.Inherited
+
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Retention(AnnotationRetention.RUNTIME)
@@ -8,6 +10,7 @@ annotation class Mappable {
     @MustBeDocumented
     @Target(AnnotationTarget.FIELD, AnnotationTarget.FILE)
     @Retention(AnnotationRetention.RUNTIME)
+    @Inherited
     annotation class Key(
         val value: String
     )
